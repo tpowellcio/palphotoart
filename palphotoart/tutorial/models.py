687ebff52d1165d3517c496233eae499ab085486
Troy Powell <thepalsrus@gmail.com>
2014-07-21 06:51:51 -0600

    working on server now

--- palphotoart/tutorial/models.py
+++ palphotoart/tutorial/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel    
+
+class BlogPage(Page):
+    body = RichTextField()
+    date = models.DateField("Post date")
+    search_name = "Blog Page"
+
+    indexed_fields = ('body', )
+
+BlogPage.content_panels = [
+    FieldPanel('title', classname="full title"),
+    FieldPanel('date'),
+    FieldPanel('body', classname="full"),
+]
